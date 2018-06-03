6d14cbfe234592f6cefe399f2d7221b575904e74
Unknown <diegopereiracalcada@gmail.com>
2018-06-03 19:50:19 -0300

    ajustes js e css

--- src/App.js
+++ src/App.js
@@ -26,16 +26,53 @@ class Header extends React.Component {
   }
 }
 
+function fecharChamado(chamado) {
+  console.log("Iniciando fechamento de chamado...");
+  console.log("chamado to persist:");
+  console.log(chamado);
+
+}
+
 class ItemChamado extends React.Component {
+
+  constructor(props) {
+    console.log("ItemChamado props:");
+    console.log(props);
+    super(props);
+    this.state = {
+      _id_: props._id_,
+      idCliente: props.idCliente,
+      desc: props.desc,
+      dtAbertura: props.dtAbertura
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    console.log("onclick:");
+    console.log(target);
+    console.log(value);
+    console.log(name);
+
+    fecharChamado(this.props);
+
+  }
+
   render() {
     return (
       <li className="li-chamado">
         <div className="infs-chamado">
-          <p className="nome-cliente">Qualityfisio</p>
-          <p className="desc-chamado">Pc nao liga</p>
-          <p>Aberto em<span className="dt-abertura">21/21/2121</span></p>
+          <input type="hidden" name="idCliente" value={this.props.idCliente} />
+          <p className="nome-cliente">{this.props.idCliente}</p>
+          <p className="desc-chamado">{this.props.desc}</p>
+          <p>Aberto em <span className="dt-abertura">{this.props.dtAbertura}</span></p>
         </div>
-        <button className="btn-fechar-chamado"></button>
+        <button
+          onClick={this.handleOnClick}
+          className="waves-effect waves-light btn btn-fechar-chamado">Fechar</button>
       </li>
     );
   }
@@ -45,9 +82,9 @@ class ListaChamados extends React.Component {
   render() {
     return (
       <ul className="ul-chamados">
-        <ItemChamado />
-        <ItemChamado />
-        <ItemChamado />
+        <ItemChamado _id_="801" idCliente="1" desc="desccham1" dtAbertura="21/01/2018" />
+        <ItemChamado _id_="802" idCliente="2" desc="desccham2" dtAbertura="21/02/2018" />
+        <ItemChamado _id_="803" idCliente="3" desc="desccham3" dtAbertura="21/03/2018" />
       </ul>
     );
   }
@@ -84,7 +121,6 @@ class App extends React.Component {
     );
   }
 }
-
 
 export default App;
 
