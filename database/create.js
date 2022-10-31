fca1f5879dd6a64369711817c1d32c39a377d0ad
diegopereiracalcadadev <32773695+diegopereiracalcadadev@users.noreply.github.com>
2022-10-31 18:07:51 -0300

    teste

--- database/create.js
+++ database/create.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost/mydb";
 
+
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
